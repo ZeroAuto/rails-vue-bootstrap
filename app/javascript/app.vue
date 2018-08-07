90c2d909a3722a52d1f801f52fb7b0927a9440f6
Mike James <mikejames386@gmail.com>
2018-08-07 09:08:52 -0500

    remove errors in markup

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
     <div class="container">
-      <h1>{{errors}}</h1>
       <div class="row">
         <div class="col-md-9">
           <b-table striped hover :items="players" :fields="checkedFields"></b-table>
