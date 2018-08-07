1e67c2ca59656178fa62f4e28cf4f2b8bcbf27f7
Mike James <mikejames386@gmail.com>
2018-08-07 00:55:16 -0500

    import saved filters to frontend

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -3,7 +3,7 @@
     <div class="container">
       <div class="row">
         <div class="col-md-9">
-          <b-table striped hover :items="players['players']" :fields="checkedFields"></b-table>
+          <b-table striped hover :items="players" :fields="checkedFields"></b-table>
         </div>
         <div class="col-md-3">
           <div v-for="field in fields" class="form-group">
@@ -11,6 +11,9 @@
             <label class="form-check-label" >{{field.key}}</label>
           </div>
           <button type="button" class="btn btn-primary">Save Filter</button>
+          <div v-for="filter in savedFilters">
+            <b-link href="#">{{filter.created_at}}</b-link>
+          </div>
         </div>
       </div>
     </div>
@@ -54,13 +57,34 @@ export default {
   created: function() {
     axios.get('http://localhost:5000/api/players')
     .then(response => {
-      this.players = response.data
+      this.players = response.data['players']
     })
     .catch(e => {
       this.errors.push(e)
     })
 
+    axios.get('http://localhost:5000/api/filters')
+    .then(response => {
+      this.savedFilters = response.data['filters']
+    })
+    .catch(e => {
+      console.log(e);
+      this.errors.push(e)
+    })
+
     this.checkedFields = this.fields
+  },
+
+  methods: {
+    getSavedFilters() {
+      axios.get('http://localhost:5000/api/filters')
+      .then(response => {
+        this.savedFilters = repsonse.data['filters']
+      })
+      .catch(e => {
+        this.errors.push(e)
+      })
+    }
   }
 }
 </script>
