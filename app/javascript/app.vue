5a6c2a957ba6a21bf6a65d35a05ed68502a125f3
Mike James <mikejames386@gmail.com>
2018-08-06 22:03:38 -0500

    load data into frontend

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,15 +1,29 @@
 <template>
   <div id="app">
     <p>{{ message }}</p>
+    <p v-for="player in players['players']">{{player.name}}</p>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   data: function () {
     return {
-      message: "Hello Vue!"
+      message: "Hello Vue!",
+      players: [],
+      errors: []
     }
+  },
+
+  created: function() {
+    axios.get('http://localhost:5000/api/players')
+    .then(response => {
+      this.players = response.data
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })
   }
 }
 </script>
